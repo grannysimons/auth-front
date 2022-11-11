9ecd16c0344ef9c0d546b0a03e5631b429825aa2
Mariona Roca <marionaroca@MacBook-MacBook-Pro-de-Mariona.local>
2022-11-11 01:16:27 +0100

    react_app_api_url

--- src/context/auth.context.jsx
+++ src/context/auth.context.jsx
@@ -1,7 +1,7 @@
 import { createContext, useState } from 'react';
 import axios from 'axios';
 import { useEffect } from 'react';
-const BACK_URL = process.env.REACT_APP_REACT_APP_API_URL || "http://localhost:5005/api/auth";
+const BACK_URL = process.env.REACT_APP_API_URL || "http://localhost:5005/api/auth";
 
 const authContext = createContext();
 
